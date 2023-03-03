4ccdd335853d1a0b12062c9f35aa8c5f7ce6694a
Zokster9 <zoran00.zb@gmail.com>
2023-03-03 14:21:11 +0100

    Add placeholder header, footer and body

---
+++ library-valcon-internship/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <h1>Header Component</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ library-valcon-internship/src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+import './Layout.css'
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div className='Layout'>Layout Component</div>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
