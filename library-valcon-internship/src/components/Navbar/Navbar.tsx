6db1c4d68d2e3e4e1808aaa61e9c6b416279dffc
Zokster9 <zoran00.zb@gmail.com>
2023-03-06 13:30:04 +0100

    Rename Footer to Navbar and add styles for desktop

--- library-valcon-internship/src/components/Navbar/Navbar.tsx
+++ library-valcon-internship/src/components/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import home from '../../assets/icons/home-icon.svg'
 import profile from '../../assets/icons/profile-icon.svg'
 import hamburgerMenuIcon from '../../assets/icons/hamburger-menu-icon.svg'
-import './Footer.css'
+import './Navbar.css'
 
 interface FooterProps {
   handleMenuClick: () => void
 }
 
-const Footer = ({ handleMenuClick }: FooterProps) => {
+const Navbar = ({ handleMenuClick }: FooterProps) => {
   return (
     <nav className='Footer'>
       <a className='footer-link' href=''>
@@ -19,8 +19,17 @@ const Footer = ({ handleMenuClick }: FooterProps) => {
       <button className='footer-link' onClick={() => handleMenuClick()}>
         <img src={hamburgerMenuIcon} alt='moreOptions-icon' />
       </button>
+      <a href='/' className='footer-options-link'>
+        Option 1
+      </a>
+      <a href='/' className='footer-options-link'>
+        Option 2
+      </a>
+      <a href='/' className='footer-options-link'>
+        Option 3
+      </a>
     </nav>
   )
 }
 
-export default Footer
+export default Navbar
