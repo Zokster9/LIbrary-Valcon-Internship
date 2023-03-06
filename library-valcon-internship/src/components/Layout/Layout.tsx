ebbf6b7ad53e166bc6a22c7a6f329e23e796bdee
Zokster9 <zoran00.zb@gmail.com>
2023-03-06 16:33:48 +0100

    Import css in the bottom

--- library-valcon-internship/src/components/Layout/Layout.tsx
+++ library-valcon-internship/src/components/Layout/Layout.tsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import Header from '../Header/Header'
 import MobileSidebar from '../MobileSidebar/MobileSidebar'
-import './Layout.css'
 import AppRouter from '../AppRouter/AppRouter'
+import './Layout.css'
 
 const Layout = () => {
   const [ isVisible, setIsVisible ] = useState<boolean>(false)
