4c6b029577977fec2dd5d7c7d4b8200411cf8e76
Zokster9 <zoran00.zb@gmail.com>
2023-03-08 15:31:31 +0100

    Create floating action button

--- library-valcon-internship/src/pages/HomePage/HomePage.tsx
+++ library-valcon-internship/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,19 @@
+import { NavLink } from 'react-router-dom'
+
+import addIcon from '../../assets/icons/add-icon.svg'
 import './HomePage.css'
 
-const HomePage = () => <div className='HomePage'>HomePage Component</div>
+const HomePage = () => {
+  return (
+    <div className='homePage'>
+      <h3>No books currently available</h3>
+      <button className='fab'>
+        <NavLink to='/create-book'>
+          <img src={addIcon} alt='Add icon' />
+        </NavLink>
+      </button>
+    </div>
+  )
+}
 
 export default HomePage
