7e00b765b24fbd90a98429a80f803276518937ab
Zokster9 <zoran00.zb@gmail.com>
2023-03-14 11:54:02 +0100

    Extract role key for jwt decode

--- library-valcon-internship/src/services/AxiosConfiguration.ts
+++ library-valcon-internship/src/services/AxiosConfiguration.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Token from '../models/Token'
 
 export const baseUrl: string = process.env.REACT_APP_API_BASE_URL ? process.env.REACT_APP_API_BASE_URL : ''
+export const ROLE_KEY = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
 
 export const configureAxiosRequestInterceptors = () => {
   axios.interceptors.request.use(
