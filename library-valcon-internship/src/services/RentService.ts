e7e5defcc68e8ba65a01c093ae5c2c58a5604455
Zokster9 <zoran00.zb@gmail.com>
2023-03-21 12:06:51 +0100

    Change api link to template literal

--- library-valcon-internship/src/services/RentService.ts
+++ library-valcon-internship/src/services/RentService.ts
@@ -4,13 +4,13 @@ import BookRentHistoryResponse from '../models/responses/BookRentHistoryResponse
 import { baseUrl } from './AxiosConfiguration'
 
 export const rentBook = async (bookId: string) => {
-  return axios.post(baseUrl + `api/Rental/rent/${bookId}`)
+  return axios.post(`${baseUrl}api/Rental/rent/${bookId}`)
 }
 
 export const getBookRentHistory = async (bookId: string) => {
-  return axios.get<BookRentHistoryResponse[]>(baseUrl + `api/Rental/book-history/${bookId}`)
+  return axios.get<BookRentHistoryResponse[]>(`${baseUrl}api/Rental/book-history/${bookId}`)
 }
 
 export const returnBook = async (bookId: string, userId: number) => {
-  return axios.post(baseUrl + `api/Rental/return/${bookId}/${userId}`)
+  return axios.post(`${baseUrl}api/Rental/return/${bookId}/${userId}`)
 }
